9b3ff93d39b353f6cdd688057be75d0b226e7028
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2024-11-04 18:46:43 +0000

    Fixing table_one frequencies calculation

--- analysis/practice_measures.py
+++ analysis/practice_measures.py
@@ -2,6 +2,9 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 
+# Date specifications
+study_start_date = "2022-01-03"
+
 # Load and format data
 measures = pd.read_csv("output/patient_measures/winter_pressure_measures.csv.gz")
 # Reformat ethnicity data
@@ -54,13 +57,29 @@ practice_df.drop(columns=cols_to_convert, inplace=True)
 practice_df.to_csv("output/practice_measures/practice_measures.csv.gz")
 
 # Create frequency table
-measures_at_start = measures[measures['interval_start'] == '2022-01-03']
+measures_at_start = measures[(measures['interval_start'] == study_start_date) & (measures['measure'] == 'appointments_in_interval')]
 # Extract demographic variables
 table_one_vars = measures.columns[measures.columns.get_loc('denominator') + 1:]
 table_one = {}
+# Iterate over all the demographic variables we want in table one
 for var in table_one_vars:
-    table_one[var] = (measures_at_start[var].value_counts(normalize=True)
-                      .rename_axis(f'Value')
-                      .reset_index(name='Propn'))
-table_one = pd.concat(table_one)
-table_one.to_csv('output/patient_measures/frequency_table.csv')
+    # Create an binary matrix composed of indicator variables representing the categorical value for each group (M)
+    # Multiply this matrix by the vector of denominators representing the size of the given group (d)
+    # Sum to get the total denominator value from all the groups (t = Sum(M x d))
+    table_one[var] = pd.get_dummies(measures_at_start[var]).multiply(measures_at_start['denominator'], axis=0).sum().reset_index()
+    table_one[var].columns = ['value', 'count']
+    table_one[var]['prop'] = table_one[var]['count']/table_one[var]['count'].sum()
+
+# Initialize an empty list to hold formatted DataFrames
+formatted_list = []
+
+# Loop over each item in the dictionary
+for key, df in table_one.items():
+    # Add a column for the category (e.g., age, sex, ethnicity)
+    df['Category'] = key
+    # Append the DataFrame to the list
+    formatted_list.append(df[['Category', 'value', 'count', 'prop']])
+
+# Concatenate all DataFrames into one
+result_df = pd.concat(formatted_list, axis=0, ignore_index=True)
+result_df.to_csv('output/patient_measures/frequency_table.csv', index=False)
