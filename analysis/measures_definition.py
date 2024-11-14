b62ac6ae72f4fee964e7d7df4c948c2ba4adf325
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2024-11-14 12:31:33 +0000

    Appointment stratification by patient co-morbidity (#16)
    Subroup analysis of appointment rates by patient co-morbidity. Decision not to implement through a function
    Code discussed here:
    https://bennettoxford.slack.com/archives/C33TWNQ1J/p1730463121518129
    Closes #2

--- analysis/measures_definition.py
+++ analysis/measures_definition.py
@@ -1,5 +1,5 @@
 # Task one: generate weekly appointment rates for elegible patients
-# Task two: stratify by selected patient characteristics: age, sex, ethnicity, imd, care home residency, vaccination status & co-morbdiity (need to discuss which)
+# Task two: stratify by selected patient characteristics: age, sex, ethnicity, imd, care home residency, vaccination status & co-morbdiity
 
 from ehrql import case, codelist_from_csv, create_dataset, days, weeks, years, when, INTERVAL, create_measures
 from ehrql.tables.core import medications, patients
@@ -58,6 +58,25 @@ med_dict ={
     "analgesic_transdermal":"codelists/opensafely-opioid-containing-medicines-transdermal-excluding-drugs-for-substance-misuse-dmd.csv",
 }
 med_dict = create_codelist_dict(med_dict)
+
+# Co-morbidity codelists:
+comorbid_dict = {
+    "chronic_resp": "codelists/nhsd-primary-care-domain-refsets-crdatrisk1_cod.csv",
+    "copd": "codelists/nhsd-primary-care-domain-refsets-copd_cod.csv",
+    "copd_res": "codelists/nhsd-primary-care-domain-refsets-copdres_cod.csv",
+    "asthma": "codelists/nhsd-primary-care-domain-refsets-ast_cod.csv",
+    "asthma_res": "codelists/nhsd-primary-care-domain-refsets-astres_cod.csv",
+    "diabetes": "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv",
+    "diabetes_res": "codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv",
+    "htn": "codelists/nhsd-primary-care-domain-refsets-hyp_cod.csv",
+    "htn_res": "codelists/nhsd-primary-care-domain-refsets-hypres_cod.csv",
+    "depres": "codelists/nhsd-primary-care-domain-refsets-depr_cod.csv",
+    "depres_res": "codelists/nhsd-primary-care-domain-refsets-depres_cod.csv",
+    "mental_health": "codelists/qcovid-has_severe_mental_illness.csv",
+    "neuro": "codelists/primis-covid19-vacc-uptake-cns_cov.csv",
+    "immuno_sup": "codelists/nhsd-immunosupression-pcdcluster-snomed-ct.csv"
+}
+comorbid_dict = create_codelist_dict(comorbid_dict)
 
 # exclusion criteria ---
 
@@ -131,7 +150,7 @@ practice_id = (practice_registrations.for_patient_on(INTERVAL.start_date)
 region = (practice_registrations.for_patient_on(INTERVAL.start_date)
           .practice_nuts1_region_name)
 
-# Vaccination against flue or covid in the last 12 months
+# Vaccination against flu or covid in the last 12 months
 vax_status = {}
 for disease in ['influenza', 'covid']:
     vax_status[disease] = (vaccinations.where((vaccinations
@@ -141,6 +160,161 @@ for disease in ['influenza', 'covid']:
                                         .date
                                         .is_on_or_between(INTERVAL.start_date - years(1), INTERVAL.start_date))
                                         .exists_for_patient())
+
+# Co-morbidity
+
+# Chronic resp disease (no resolution codelist). True if disease developed before interval start, else False.
+comorbid_chronic_resp = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["chronic_resp"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .exists_for_patient()
+)
+
+# COPD (with resolution codelist). 
+## Last COPD diagnosis date before interval start
+comorbid_copd_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["copd"]) &
+        clinical_events.date.is_on_or_before(INTERVAL.start_date))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+
+# Last COPD resolution date before interval start
+comorbid_copd_res_date_last = ( 
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["copd_res"]) &
+        clinical_events.date.is_on_or_before(INTERVAL.start_date))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+
+## True if COPD developed before interval start & never resolved OR resolved but recurred before the interval; else False
+comorbid_copd = (
+    comorbid_copd_date_last.is_not_null() & 
+    (comorbid_copd_res_date_last.is_null() | (comorbid_copd_res_date_last < comorbid_copd_date_last))
+).when_null_then(False)
+
+# Asthma (with resolution codelist).
+## Last asthma diagnosis date before interval start
+comorbid_asthma_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["asthma"]) &
+        clinical_events.date.is_on_or_before(INTERVAL.start_date))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+
+## Last asthma resolution date before interval start
+comorbid_asthma_res_date_last = ( 
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["asthma_res"]) &
+        clinical_events.date.is_on_or_before(INTERVAL.start_date))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+
+## True if asthma developed before interval start & never resolved OR resolved but recurred before the interval; else False
+comorbid_asthma = (
+    comorbid_asthma_date_last.is_not_null() & 
+    (comorbid_asthma_res_date_last.is_null() | (comorbid_asthma_res_date_last < comorbid_asthma_date_last))
+).when_null_then(False)
+
+# Diabetes (with resolution codelist)
+## Last diabetes diagnosis date before interval start
+comorbid_dm_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["diabetes"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .sort_by(clinical_events.date)
+                          .last_for_patient()
+                          .date
+)
+
+## Last diabetes resolution date before interval start
+comorbid_dm_res_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["diabetes_res"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .sort_by(clinical_events.date)
+                          .last_for_patient()
+                          .date
+)
+
+## True if diabetes developed before interval start & never resolved OR resolved but recurred before the interval start; else False
+comorbid_dm = (
+    comorbid_dm_date_last.is_not_null() &
+    (comorbid_dm_res_date_last.is_null() | (comorbid_dm_res_date_last < comorbid_dm_date_last))
+).when_null_then(False)
+
+# Hypertension (with resolution codelist)
+## Last hypertension diagnosis date before interval start
+comorbid_htn_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["htn"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .sort_by(clinical_events.date)
+                          .last_for_patient()
+                          .date
+)
+
+## Last hypertension resolution date before interval start
+comorbid_htn_res_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["htn_res"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .sort_by(clinical_events.date)
+                          .last_for_patient()
+                          .date
+)
+
+## True if hypertension developed before interval start & never resolved OR resolved but recurred before the interval start; else False
+comorbid_htn = (
+    comorbid_htn_date_last.is_not_null() &
+    (comorbid_htn_res_date_last.is_null() | (comorbid_htn_res_date_last < comorbid_htn_date_last))
+).when_null_then(False)
+
+# Depression (with resolution codelist)
+## Last depression diagnosis date before interval start
+comorbid_depres_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["depres"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .sort_by(clinical_events.date)
+                          .last_for_patient()
+                          .date
+)
+
+## Last depression resolution date before interval start
+comorbid_depres_res_date_last = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["depres_res"]) &
+                          clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .sort_by(clinical_events.date)
+                          .last_for_patient()
+                          .date
+)
+
+## True if depression developed before interval start & never resolved OR resolved but recurred before the interval start; else False
+comorbid_depres = (
+    comorbid_depres_date_last.is_not_null() &
+    (comorbid_depres_res_date_last.is_null() | (comorbid_depres_res_date_last < comorbid_depres_date_last))
+).when_null_then(False)
+
+# Chronic mental health disease (no resolution codelist). True if disease developed before interval start; else False
+comorbid_mh = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["mental_health"])
+                          & clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .exists_for_patient()
+)
+
+# Chronic neurological disease (no resolution codelist). True if disease developed before interval start; else False
+comorbid_neuro = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["neuro"])
+                          & clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .exists_for_patient()
+)
+
+# Immunosupression (no resolution codelist). True if disease developed before interval start; else False
+comorbid_immuno = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(comorbid_dict["immuno_sup"])
+                          & clinical_events.date.is_on_or_before(INTERVAL.start_date))
+                          .exists_for_patient()
+)
 
 # Measures ---
 measures_to_add = {}
@@ -252,6 +426,15 @@ measures.define_defaults(
         "region": region,
         "rur_urb_class": rur_urb_class,
         "practice_pseudo_id": practice_id,
+        "comorbid_chronic_resp": comorbid_chronic_resp,
+        "comorbid_copd": comorbid_copd,
+        "comorbid_asthma": comorbid_asthma,
+        "comorbid_dm": comorbid_dm,
+        "comorbid_htn": comorbid_htn,
+        "comorbid_depres": comorbid_depres,
+        "comorbid_mh": comorbid_mh,
+        "comorbid_neuro": comorbid_neuro,
+        "comorbid_immuno": comorbid_immuno
 #        "vax_flu_12m": vax_status['influenza'], Need to check vaccine target disease is correct
 #        "vax_covid_12m": vax_status['covid']
     },
