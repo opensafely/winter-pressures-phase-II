1deae0e7b9c1fa626e197d105de0a1c376bba124
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2024-12-13 16:27:52 +0000

    Atam/separate practices (#34)
    * Updated age cats for dataset
    * Split measures into patient_measures & practice_measures. Hashed out ethnicity

--- analysis/dataset.py
+++ analysis/dataset.py
@@ -49,10 +49,12 @@ has_region = practice_registrations.for_patient_on(study_start_date).practice_nu
 age = age_at_interval_start
 age_group = case(
     when((age >= 0) & (age < 5)).then("preschool"),
-    when((age >= 5) & (age <18)).then("school"),
-    when((age >= 18) & (age < 65)).then("adult"),
-    when((age >= 65) & (age < 80)).then("retired"),
-    when((age >= 80) & (age < 111)).then("elderly"),
+    when((age >= 5) & (age < 12)).then("primary-school"),
+    when((age >= 12) & (age < 18)).then("secondary-school"),
+    when((age >= 18) & (age < 40)).then("adult<40"),
+    when((age >= 40) & (age < 65)).then("adult<65"),
+    when((age >= 65) & (age < 80)).then("adult<80"),
+    when((age >= 80) & (age < 111)).then("adult>80")
 )
 
 # Ethnicity
