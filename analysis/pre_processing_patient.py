f52eed194e79e0306dcfa019a6975ea0f26aa737
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2025-03-21 16:13:52 +0000

    Ungrouped measures & patient df (#91)
    * Aggregate patient dfs 1
    
    * Rebase to main (#88)
    
    * Fixing practice-level characteristics aggregation
    
    * Improving efficiency of aggregation
    
    * Using .csv.gz for test output for ease of opening
    
    * Iteratively process each interval
    
    * Patient-df processing
    * Ungrouped measures processing
    
    Co-authored-by: Arina Tamborska <143546177+atamborska@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>

--- analysis/pre_processing_patient.py
+++ analysis/pre_processing_patient.py
@@ -1,7 +1,8 @@
 #TODO:
-# Split into two outputs: 1) demographic-level 2) comoorbidity-level
-# Check if frequency table code at end needs updating
-# Change output to arrow files
+# Troubleshoot increasing processed_df size
+# Set up a seperate patient_comorbid processing
+# Update frequency table code at the end
+
 import pandas as pd
 from scipy import stats
 import numpy as np
@@ -9,6 +10,7 @@ import argparse
 from datetime import datetime, timedelta
 import os
 from utils import generate_annual_dates, log_memory_usage, replace_nums
+import pyarrow.feather as feather
 from wp_config_setup import *
 
 # -------- Set up variables ----------------------------------
@@ -19,14 +21,13 @@ if test == True:
 else:
     dates = generate_annual_dates(2016, '2024-07-31')
     suffix = ""
-flags = ["practice_measures"]
 study_start_date = dates[0]
 
 log_memory_usage(label="Before loading data")
 
 # -------- Patient measures processing ----------------------------------
 
-patient_df_dict = {}
+patient_dataframes = []
 
 # Load and format data for each interval
 for date in dates:
@@ -38,56 +39,62 @@ for date in dates:
         'comorbid_mh': 'bool', 'comorbid_neuro': 'bool', 'comorbid_immuno': 'bool', 'vax_flu_12m': 'bool',
         'vax_covid_12m': 'bool', 'vax_pneum_12m': 'bool'
     }
-    # Load data for each interval and each flag
-    patient_df_dict[date] = pd.read_csv(f"output/patient_measures/patient_measures_{date}{suffix}.csv.gz",
+    # Load data for each interval
+    df = pd.read_csv(f"output/patient_measures/patient_measures_{date}{suffix}.csv.gz",
                                         dtype=dtype_dict, true_values=["T"], false_values=["F"])
+    
     log_memory_usage(label=f"After loading patient {date}")
         
     # Collapse rur_urb_class to two categories: #1 for urban and #2 for rural
-    patient_df_dict[date]['rur_urb_class'] = patient_df_dict[date]['rur_urb_class'].apply(
+    df['rur_urb_class'] = df['rur_urb_class'].apply(
         lambda x: '1' if x in ['1', '2', '3', '4'] else ('2' if x in ['5', '6', '7', '8'] else np.nan)
         )
     # print type of each column
-    print(f"Data types of input: {patient_df_dict[date].dtypes}")
-    print(f"Before grouping shape: {patient_df_dict[date].shape}")
+    print(f"Data types of input: {df.dtypes}")
+    print(f"Before grouping shape: {df.shape}")
     # count without 0 numerator
-    print(f"count without 0 numerator: {patient_df_dict[date][(patient_df_dict[date]['numerator'] > 0)].shape}")
+    print(f"count without 0 numerator: {df[(df['numerator'] > 0)].shape}")
     # count without nan numerator
-    print(f"count without nan numerator: {patient_df_dict[date][(patient_df_dict[date]['numerator'].notna())].shape}")
+    print(f"count without nan numerator: {df[(df['numerator'].notna())].shape}")
     # count without 0 list_size
-    print(f"count without 0 denominator: {patient_df_dict[date][(patient_df_dict[date]['denominator'] > 0)].shape}")
+    print(f"count without 0 denominator: {df[(df['denominator'] > 0)].shape}")
     # count without nan list_size
-    print(f"count without nan denominator: {patient_df_dict[date][(patient_df_dict[date]['denominator'].notna())].shape}")
+    print(f"count without nan denominator: {df[(df['denominator'].notna())].shape}")
 
     # Perform efficient groupby and aggregation
     print('GROUPING AND AGGREGATING')
     # Aggregate by the demographic columns
-    patient_df_dict[date] = patient_df_dict[date].groupby(['measure', 'interval_start', 'age' , 'sex', 'ethnicity', 'imd_quintile', 
+    df = df.groupby(['measure', 'interval_start', 'age' , 'sex', 'ethnicity', 'imd_quintile', 
                                                            'carehome', 'region', 'rur_urb_class']).agg(
         numerator = ("numerator", "sum"),
         list_size=("denominator", "sum")
     ).reset_index()
 
     # count without 0 numerator
-    print(f"count without 0 numerator: {patient_df_dict[date][(patient_df_dict[date]['numerator'] > 0)].shape}")
+    print(f"count without 0 numerator: {df[(df['numerator'] > 0)].shape}")
     # count without nan numerator
-    print(f"count without nan numerator: {patient_df_dict[date][(patient_df_dict[date]['numerator'].notna())].shape}")
+    print(f"count without nan numerator: {df[(df['numerator'].notna())].shape}")
     # count without 0 list_size
-    print(f"count without 0 list_size: {patient_df_dict[date][(patient_df_dict[date]['list_size'] > 0)].shape}")
+    print(f"count without 0 list_size: {df[(df['list_size'] > 0)].shape}")
     # count without nan list_size
-    print(f"count without nan list_size: {patient_df_dict[date][(patient_df_dict[date]['list_size'].notna())].shape}")
+    print(f"count without nan list_size: {df[(df['list_size'].notna())].shape}")
     
     # Drop rows with 0 list_size or nan list_size
-    patient_df_dict[date] = patient_df_dict[date][(patient_df_dict[date]['list_size'] > 0) & (patient_df_dict[date]['list_size'].notna())]
-    print(f"After grouping shape: {patient_df_dict[date].shape}")
+    df = df[(df['list_size'] > 0) & (df['list_size'].notna())]
+    print(f"After grouping shape: {df.shape}")
 
-    print("After grouping:", patient_df_dict[date].shape)
+    print("After grouping:", df.shape)
 
-# Concatenate all DataFrames into one
-patient_df = pd.concat(patient_df_dict.values(), ignore_index=True)
+    patient_dataframes.append(df)
+    del(df)
+    log_memory_usage(label=f"After deletion of patient df")
+
+# Save processed file
+patient_df = pd.concat(patient_dataframes)
+del patient_dataframes
 print(f"Data types of input: {patient_df.dtypes}")
-del patient_df_dict
-log_memory_usage(label=f"After deletion of practices_dict")
+log_memory_usage(label=f"After deletion of patient_dataframes")
+
 # Replace numerical values with string values
 patient_df = replace_nums(patient_df, replace_ethnicity=True, replace_rur_urb=True)
 
@@ -95,38 +102,37 @@ patient_df = replace_nums(patient_df, replace_ethnicity=True, replace_rur_urb=Tr
 if test:
     patient_df.to_csv(f"output/patient_measures/proc_patient_measures_test.csv.gz")
 else:
-    patient_df.to_csv(f"output/patient_measures/proc_patient_measures.csv.gz")
+    feather.write_feather(patient_df, f"output/patient_measures/proc_patient_measures.arrow")
 
 # -------- Frequency table generation ----------------------------------
-'''
-# Create frequency table
-patient_df_at_start = patient_df[(patient_df['interval_start'] == study_start_date) & (patient_df['measure'] == 'seen_in_interval')]
-# Extract demographic variables
-table_one_vars = patient_df.columns[patient_df.columns.get_loc('denominator') + 1:]
-table_one = {}
-# Iterate over all the demographic variables we want in table one
-for var in table_one_vars:
-    # Create an binary matrix composed of indicator variables representing the categorical value for each group (M)
-    # Multiply this matrix by the vector of denominators representing the size of the given group (d)
-    # Sum to get the total denominator value from all the groups (t = Sum(M x d))
-    table_one[var] = pd.get_dummies(patient_df_at_start[var]).multiply(patient_df_at_start['denominator'], axis=0).sum().reset_index()
-    table_one[var].columns = ['value', 'count']
-    table_one[var]['prop'] = table_one[var]['count']/table_one[var]['count'].sum()
+
+# # Create frequency table
+# patient_df_at_start = patient_df[(patient_df['interval_start'] == study_start_date) & (patient_df['measure'] == 'seen_in_interval')]
+# # Extract demographic variables
+# table_one_vars = patient_df.columns[patient_df.columns.get_loc('denominator') + 1:]
+# table_one = {}
+# # Iterate over all the demographic variables we want in table one
+# for var in table_one_vars:
+#     # Create an binary matrix composed of indicator variables representing the categorical value for each group (M)
+#     # Multiply this matrix by the vector of denominators representing the size of the given group (d)
+#     # Sum to get the total denominator value from all the groups (t = Sum(M x d))
+#     table_one[var] = pd.get_dummies(patient_df_at_start[var]).multiply(patient_df_at_start['denominator'], axis=0).sum().reset_index()
+#     table_one[var].columns = ['value', 'count']
+#     table_one[var]['prop'] = table_one[var]['count']/table_one[var]['count'].sum()
 
 
-# Initialize an empty list to hold formatted DataFrames
-formatted_list = []
+# # Initialize an empty list to hold formatted DataFrames
+# formatted_list = []
 
-# Loop over each item in the dictionary
-for key, df in table_one.items():
-    # Add a column for the category (e.g., age, sex, ethnicity)
-    df['Category'] = key
-    # Append the DataFrame to the list
-    formatted_list.append(df[['Category', 'value', 'count', 'prop']])
+# # Loop over each item in the dictionary
+# for key, df in table_one.items():
+#     # Add a column for the category (e.g., age, sex, ethnicity)
+#     df['Category'] = key
+#     # Append the DataFrame to the list
+#     formatted_list.append(df[['Category', 'value', 'count', 'prop']])
 
-# Concatenate all DataFrames into one
-result_df = pd.concat(formatted_list, axis=0, ignore_index=True)
+# # Concatenate all DataFrames into one
+# result_df = pd.concat(formatted_list, axis=0, ignore_index=True)
 
-# Save processed file
-result_df.to_csv(f'output/patient_measures/frequency_table{suffix}.csv', index=False)
-'''
+# # Save processed file
+# result_df.to_csv(f'output/patient_measures/frequency_table{suffix}.csv', index=False)
---
+++ analysis/pre_processing_ungrouped.py
@@ -0,0 +1,78 @@
+# TODO:
+# Instead of loading all data at once, iterate over each date and process it
+import pandas as pd
+from scipy import stats
+import numpy as np
+import argparse
+from datetime import datetime, timedelta
+import os
+from utils import generate_annual_dates, log_memory_usage, replace_nums
+import pyarrow.feather as feather
+from wp_config_setup import *
+
+# -------- Set up variables ----------------------------------
+
+if test == True:
+    dates = ["2016-08-10"]
+    suffix = "_test"
+else:
+    dates = generate_annual_dates(2016, '2024-07-31')
+    suffix = ""
+study_start_date = dates[0]
+
+log_memory_usage(label="Before loading data")
+
+# -------- Unstratified measures processing ----------------------------------
+
+# Initialize a list to store processed data
+proc_dataframes = []
+
+# Load and format data for each interval
+for date in dates:
+
+    dtype_dict = {
+    "measure": "category",
+    "interval_start": "category",
+    "interval_end": "category",
+    "ratio": "float32",
+    "numerator": "int64",
+    "denominator": "int64",
+    "age": "category",
+    "sex": "category",
+    "ethnicity": "object",
+    "imd_quintile": "int8", # range of int8 is -128 to 127
+    "carehome": "category",
+    "region": "category",
+    "rur_urb_class": "Int8", # nullable integer type (not 'I' not 'i')
+    "practice_pseudo_id": "int16", # range of int16 is -32768 to 32767
+    }
+    df = pd.read_csv(f"output/practice_measures/practice_measures_{date}{suffix}.csv.gz",
+                                         dtype = dtype_dict, true_values=["T"], false_values=["F"])
+
+    log_memory_usage(label=f"After loading practice {date}")
+
+    # print type of each column
+    print(f"Data types of input: {df.dtypes}")
+
+    # Perform efficient groupby and aggregation
+    df = (
+        df.groupby(["measure", "interval_start"])
+        .agg(
+            numerator=("numerator", "sum"),
+            list_size=("denominator", "sum"),
+        )
+    )
+
+    print(f"Data types of output dataframe: {df.dtypes}")
+    proc_dataframes.append(df)
+    del df
+        
+# Save processed file
+proc_df = pd.concat(proc_dataframes)
+del proc_dataframes
+
+if test:
+    proc_df.to_csv("output/ungrouped_measures/proc_ungrouped_measures_test.csv.gz")
+else:
+    feather.write_feather(proc_df, f"output/ungrouped_measures/proc_ungrouped_measures.arrow")
+
