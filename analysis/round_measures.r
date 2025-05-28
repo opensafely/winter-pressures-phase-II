af3817800b945e188d5cab54b2a0b12b42a8c165
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2025-05-28 10:53:01 +0000

    Fixing rounding

--- analysis/round_measures.r
+++ analysis/round_measures.r
@@ -8,17 +8,18 @@ library(readr)
 library(optparse)
 library(arrow)
 source("analysis/utils.r")
-source("analysis/config.R")
+source("analysis/config.r")
 
 # --------------------- Round processed measures ---------------------
 
-input_path <- glue("output/{group}_measures/proc_{group}_measures")
-output_path <- glue("output/{group}_measures/proc_{group}_measures_midpoint6")
-df_to_round <- read_write('read', args$test, input_path)
+input_path <- glue("output/{args$group}_measures/proc_{args$group}_measures")
+output_path <- glue("output/{args$group}_measures/proc_{args$group}_measures_midpoint6")
+df_to_round <- read_write('read', input_path)
+#df_to_round <- as.data.frame(df_to_round)
 
 print(glue("Before rounding: {head(df_to_round)}"))
 # Select required columns and round their values
-round_columns(df = df_to_round, cols_to_round = c('numerator', 'list_size'))
+df_to_round <- round_columns(df = df_to_round, cols_to_round = c('numerator', 'list_size'))
 # Save the rounded dataframe to a new CSV file
-read_write('write', args$test, output_path, df_to_round)
 print(glue("After rounding: {head(df_to_round)}"))
+read_write('write', output_path, df = df_to_round)
