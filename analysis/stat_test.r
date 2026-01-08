cac1504a9e9bb3028e9b33eefebb488191290add
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2026-01-08 16:18:49 +0000

    Refactoring

--- analysis/stat_test.r
+++ analysis/stat_test.r
@@ -11,16 +11,16 @@ library(stringr)
 source("analysis/utils.r")
 source("analysis/config.r")
 
-practice_counts_df <- read_write('read', glue("output/{args$group}_measures/Results_weighted_long"), file_type = 'csv')
+practice_counts_df <- read_write("read", glue("output/{args$group}_measures/Results_weighted_long"), file_type = "csv")
 
-practice_counts_df$rate_per_1000 <- (practice_counts_df$numerator_midpoint6_sum / practice_counts_df$list_size_midpoint6_sum)*1000
-practice_counts_df$major_season <- ifelse(practice_counts_df$season == 'Jun-Jul', "Summer", "Winter")
+practice_counts_df$rate_per_1000 <- (practice_counts_df$numerator_midpoint6_sum / practice_counts_df$list_size_midpoint6_sum) * 1000
+practice_counts_df$major_season <- ifelse(practice_counts_df$season == "Jun-Jul", "Summer", "Winter")
 
 # Prepare a list to store results
 results_list <- list()
 
 for (practice in unique(practice_counts_df$practice_pseudo_id)) {
-  
+
   # Subset the data for this practice
   df_sub <- filter(practice_counts_df, practice_pseudo_id == practice)
 
@@ -30,15 +30,15 @@ for (practice in unique(practice_counts_df$practice_pseudo_id)) {
     family = poisson,
     data = df_sub
   )
-  
+
   # Extract tidy results
   tidy_model <- tidy(model) %>%
     mutate(
-      rate_ratio = exp(estimate),  # exponentiate to get rate ratio
-      practice_id = practice       # add practice id
+      rate_ratio = exp(estimate), # exponentiate to get rate ratio
+      practice_id = practice # add practice id
     ) %>%
     select(practice_id, term, rate_ratio, p.value)
-  
+
   results_list[[as.character(practice)]] <- tidy_model
 }
 
@@ -71,14 +71,16 @@ df_interactions <- filter(results_table, term %in% interaction_terms) %>%
 results_wide <- df_coefs %>%
   rename_with(~ paste0(.x, "_coef"), -c(practice_id, measure)) %>%
   left_join(df_winter %>%
-              rename_with(~ paste0(.x, "_winter"), -practice_id),
-            by = "practice_id") %>%
+    rename_with(~ paste0(.x, "_winter"), -practice_id),
+  by = "practice_id"
+  ) %>%
   left_join(df_interactions %>%
-              rename_with(~ paste0(.x, "_interact"), -c(practice_id, measure)),
-            by = c("practice_id", "measure"))
+    rename_with(~ paste0(.x, "_interact"), -c(practice_id, measure)),
+  by = c("practice_id", "measure")
+  )
 
 # RR_measure in winter = RR_measure * RR_winter * RR_measure interacting with winter
-results_wide$total_RR = results_wide$rate_ratio_coef * results_wide$rate_ratio_winter * results_wide$rate_ratio_interact
+results_wide$total_RR <- results_wide$rate_ratio_coef * results_wide$rate_ratio_winter * results_wide$rate_ratio_interact
 
 # Summarise average total RR across practices for a measure
 results_agg <- results_wide %>%
@@ -87,9 +89,9 @@ results_agg <- results_wide %>%
     RR_median = median(total_RR),
     RR_lowerq = quantile(total_RR, 0.25),
     RR_uppperq = quantile(total_RR, 0.75),
-    #signif_count = sum(signif), WHICH COEFS ARE WE TESTING??
+    # signif_count = sum(signif), WHICH COEFS ARE WE TESTING??
     test_count = n()
-  ) #%>%
-  #mutate(signif_propn = (signif_count/test_count)*100)
+  ) # %>%
+# mutate(signif_propn = (signif_count/test_count)*100)
 
 read_write("write", "output/{args$group}_measures/summary_results_agg", file_type = "csv", df = results_agg)
