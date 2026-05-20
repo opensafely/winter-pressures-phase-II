0864562cf214bd1700492d1170f4266b4b32dd35
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2026-05-20 13:31:44 +0100

    Add subsets of measures

--- analysis/utils.r
+++ analysis/utils.r
@@ -149,113 +149,21 @@ summarise_demographics_rate_zero <-function(df, demo_var) {
   ggsave(output_plot_path)
 }
 
-plot_rr_timeseries_by_measure <- function(y_var, colour_var, baseline, weighting, set, representative_season = NULL) {
-    "
-    This function produces a line plot, coloured by season, faceted by measure
-    Args:
-        df: dataframe containing the results of the rate ratio calculations
-        y_var: string name of the variable to plot on the y-axis (e.g. 'RR_prev_summr_mp6')
-        colour_var: string name of the variable to colour the lines by {season / percentile / %_practices}
-        colour_label: the label for the colour legend
-        baseline: the baseline category for comparison
-    "
-    if (colour_var == "season") {
-        linetype_mapping <- c(
-            "Sep-Oct" = "dashed",
-            "Nov-Dec" = "solid",
-            "Jan-Feb" = "solid",
-            "Mar-Apr" = "dashed"
-        )
-        colour_mapping <- c(
-            "Sep-Oct" = "#1b9e77",
-            "Nov-Dec" = "#d95f02",
-            "Jan-Feb" = "#7570b3",
-            "Mar-Apr" = "#e7298a"
-        )
-    }
-    else if(colour_var == "percentile") {
-        linetype_mapping <- c(
-            "dotted",
-            "dashed",
-            "solid",
-            "dashed",
-            "dotted"
-        )
-        names(linetype_mapping) <- c(
-            glue("{y_var}_{baseline}_summr_p01"),
-            glue("{y_var}_{baseline}_summr_p25"),
-            glue("{y_var}_{baseline}_median"),
-            glue("{y_var}_{baseline}_summr_p75"),
-            glue("{y_var}_{baseline}_summr_p99")
-        )
-        colour_mapping <- c(
-            "#006eff",
-            "#006eff",
-            "#006eff",
-            "#006eff",
-            "#006eff"
-        )
-        names(colour_mapping) <- names(linetype_mapping)
-    }
-    else if (colour_var == "%_practices") {
-        linetype_mapping <- c(
-            "dashed",
-            "solid",
-            "dashed"
-        )
-        names(linetype_mapping) <- c(
-            glue("%_{y_var}_{baseline}_summr_<5%"),
-            glue("%_{y_var}_{baseline}_summr_=1"),
-            glue("%_{y_var}_{baseline}_summr_>5%")
-        )
-        colour_mapping <- c(
-            "#ff3700",
-            "#000000",
-            "#006eff"
-        )
-        names(colour_mapping) <- names(linetype_mapping)
-    }
-    if (weighting == "weighted") {
-        df_name <- paste(glue("weighted"))
-        y_col <- paste(glue("{y_var}_{baseline}_summr_mp6"))
-    }
-    else if ((weighting == "unweighted") & (colour_var == "season")) {
-        df_name <- paste(glue("unweighted_yearly_{baseline}_summer"))
-        y_col <- paste(glue("{y_var}_{baseline}_median"))
-    }
-    else if ((weighting == "unweighted") & (colour_var == "percentile")) {
-        df_name <- paste(glue("unweighted_yearly_{baseline}_summer"))
-        y_col <- paste(glue("{y_var}_{baseline}_summer"))
-    }
-    else if ((weighting == "unweighted") & (colour_var == "%_practices")) {
-        df_name <- paste(glue("unweighted_yearly_{baseline}_summer"))
-        y_col <- paste(glue("%_{y_var}_{baseline}_summer"))
-    }
-  
-    # Load results and remove any unnamed columns that may have been added during CSV export
-    results_df <- read.csv(glue("output/practice_measures_{set}/practice_measures_{set}_DUMMY/Results_{df_name}_test.csv"), check.names = FALSE) 
-    results_df <- results_df[, names(results_df) != ""]
-    
-    # Pivot percentiles if colouring by percentile
-    if (colour_var == "percentile") {
-        results_df <- results_df %>%
-            pivot_longer(cols = c(glue("{y_var}_{baseline}_summr_p01"), glue("{y_var}_{baseline}_summr_p25"), glue("{y_var}_{baseline}_median"), glue("{y_var}_{baseline}_summr_p75"), glue("{y_var}_{baseline}_summr_p99")), names_to = "percentile", values_to = as.character(y_col))
-        
-        # Percentile chart - do we need RD as well?
-        results_df <- filter(results_df, season == representative_season)
-    }
-    else if (colour_var == "%_practices") {
-      print(head(results_df))
-        results_df <- results_df %>%
-            pivot_longer(cols = c(glue("%_RR_{baseline}_summr_<5%"), glue("%_RR_{baseline}_summr_=1"), glue("%_RR_{baseline}_summr_>5%")), names_to = "%_practices", values_to = as.character(y_col))
-        
-        # Practice percentile chart - do we need RD as well?
-        results_df <- filter(results_df, season == representative_season)
-    }
-  
-    # Format measure names for readability
+format_measures_for_plotting <- function(df, set, subset) {
     if (set == "sro") {
-        results_df <- results_df %>%
+      if (subset == "tests"){
+        filtered_measures <- c("sodium_test", "alt_test", "chol_test", "sys_bp_test", "rbc_test", "hba1c_test", "cvd_10yr", "thy_test")
+      }
+      else if (subset == "other") {
+        filtered_measures <- c("asthma_review", "copd_review", "sro_prioritized", "sro_deprioritized")
+      }
+      else if (subset ==  "all") {
+        filtered_measures <- c("sodium_test", "alt_test", "chol_test", "sys_bp_test", "rbc_test", "hba1c_test", "cvd_10yr", "thy_test",
+                              "asthma_review", "copd_review", "sro_prioritized", "sro_deprioritized")
+      }
+        df <- df %>%
+            # Filter to measures of interest for plotting
+            filter(measure %in% filtered_measures) %>%
             # Rename measures
             mutate(measure = case_when(
                 measure == "sodium_test" ~ "Sodium",
@@ -274,10 +182,21 @@ plot_rr_timeseries_by_measure <- function(y_var, colour_var, baseline, weighting
             ))
     }
     else if (set == "resp") {
-        results_df <- results_df %>%
+      if (subset == "specific"){
+        filtered_measures <- c("flu_specific", "rsv_specific")
+      }
+      else if (subset == "sensitive") {
+        filtered_measures <- c("overall_resp_sensitive","flu_sensitive", "rsv_sensitive")
+      }
+      else if (subset ==  "all") {
+        filtered_measures <- c("overall_resp_sensitive", "flu_sensitive", "rsv_sensitive", "flu_specific", "rsv_specific")
+      }
+        df <- df %>%
+            # Filter to measures of interest for plotting
+            filter(measure %in% filtered_measures) %>%
             # Rename measures
             mutate(measure = case_when(
-                measure == "overall_resp_sensitive" ~ "Overall Respiratory Illness",
+                measure == "overall_resp_sensitive" ~ "Overall",
                 measure == "flu_sensitive" ~ "Flu Sensitive",
                 measure == "rsv_sensitive" ~ "RSV Sensitive",
                 measure == "flu_specific" ~ "Flu Specific",
@@ -285,8 +204,146 @@ plot_rr_timeseries_by_measure <- function(y_var, colour_var, baseline, weighting
                 TRUE ~ measure
             )) %>%
             # Reorder measures
-            mutate(measure = factor(measure, levels = c("Overall Respiratory Illness", "Flu Sensitive", "RSV Sensitive", "Flu Specific", "RSV Specific")))     
+            mutate(measure = factor(measure, levels = c("Overall", "Flu Sensitive", "RSV Sensitive", "Flu Specific", "RSV Specific")))     
     }
+    return(df)
+}
+
+format_colours_for_plotting <- function(df, colour_var, y_var = NULL, baseline = NULL) {
+  if (colour_var == "season") {
+    season_order <- c("Sep-Oct", "Nov-Dec", "Jan-Feb", "Mar-Apr", "Jun-Jul")
+
+    full_linetype_mapping <- c(
+      "Sep-Oct" = "dashed",
+      "Nov-Dec" = "solid",
+      "Jan-Feb" = "solid",
+      "Mar-Apr" = "dashed",
+      "Jun-Jul" = "solid"
+    )
+
+    full_colour_mapping <- c(
+      "Sep-Oct" = "#00fff7",
+      "Nov-Dec" = "#2f00ff",
+      "Jan-Feb" = "#76b370",
+      "Mar-Apr" = "#ffd500",
+      "Jun-Jul" = "#ff0000"
+    )
+
+    present_seasons <- intersect(season_order, unique(as.character(df$season)))
+
+    df <- df |>
+      mutate(season = factor(season, levels = present_seasons))
+
+    linetype_mapping <- full_linetype_mapping[present_seasons]
+    colour_mapping <- full_colour_mapping[present_seasons]
+  } else if (colour_var == "percentile") {
+    linetype_mapping <- c(
+      "dotted",
+      "dashed",
+      "solid",
+      "dashed",
+      "dotted"
+    )
+    names(linetype_mapping) <- c(
+      glue("{y_var}_{baseline}_summr_p01"),
+      glue("{y_var}_{baseline}_summr_p25"),
+      glue("{y_var}_{baseline}_median"),
+      glue("{y_var}_{baseline}_summr_p75"),
+      glue("{y_var}_{baseline}_summr_p99")
+    )
+    colour_mapping <- c(
+      "#006eff",
+      "#006eff",
+      "#006eff",
+      "#006eff",
+      "#006eff"
+    )
+    names(colour_mapping) <- names(linetype_mapping)
+  } else if (colour_var == "%_practices") {
+    linetype_mapping <- c(
+      "dashed",
+      "solid",
+      "dashed"
+    )
+    names(linetype_mapping) <- c(
+      glue("%_{y_var}_{baseline}_summr_<5%"),
+      glue("%_{y_var}_{baseline}_summr_=1"),
+      glue("%_{y_var}_{baseline}_summr_>5%")
+    )
+    colour_mapping <- c(
+      "#ff3700",
+      "#000000",
+      "#006eff"
+    )
+    names(colour_mapping) <- names(linetype_mapping)
+  }
+
+  list(linetype_mapping = linetype_mapping, colour_mapping = colour_mapping, df = df)
+}
+
+plot_rr_timeseries_by_measure <- function(y_var, colour_var, baseline, weighting, set, subset, representative_season = NULL) {
+    "
+    This function produces a line plot, coloured by season, faceted by measure
+    Args:
+        df: dataframe containing the results of the rate ratio calculations
+        y_var: string name of the variable to plot on the y-axis (e.g. 'RR_prev_summr_mp6')
+        colour_var: string name of the variable to colour the lines by {season / percentile / %_practices}
+        colour_label: the label for the colour legend
+        baseline: the baseline category for comparison
+    "
+    
+    if (weighting == "weighted") {
+        df_name <- paste(glue("weighted"))
+        y_col <- paste(glue("{y_var}_{baseline}_summr_mp6"))
+    }
+    else if ((weighting == "unweighted") & (colour_var == "season")) {
+        df_name <- paste(glue("unweighted_yearly_{baseline}_summer"))
+        y_col <- paste(glue("{y_var}_{baseline}_median"))
+    }
+    else if ((weighting == "unweighted") & (colour_var == "percentile")) {
+        df_name <- paste(glue("unweighted_yearly_{baseline}_summer"))
+        y_col <- paste(glue("{y_var}_{baseline}_summer"))
+    }
+    else if ((weighting == "unweighted") & (colour_var == "%_practices")) {
+        df_name <- paste(glue("unweighted_yearly_{baseline}_summer"))
+        y_col <- paste(glue("%_{y_var}_{baseline}_summer"))
+    }
+  
+    # Load results and remove any unnamed columns that may have been added during CSV export
+    results_df <- read.csv(glue("output/practice_measures_{set}/Results_{df_name}.csv"), check.names = FALSE) 
+    results_df <- results_df[, names(results_df) != ""]
+    
+    # Pivot percentiles if colouring by percentile
+    if (colour_var == "percentile") {
+        results_df <- results_df %>%
+            pivot_longer(cols = c(glue("{y_var}_{baseline}_summr_p01"), glue("{y_var}_{baseline}_summr_p25"), glue("{y_var}_{baseline}_median"), glue("{y_var}_{baseline}_summr_p75"), glue("{y_var}_{baseline}_summr_p99")), names_to = "percentile", values_to = as.character(y_col))
+        
+        # Percentile chart - do we need RD as well?
+        results_df <- filter(results_df, season == representative_season)
+    }
+    else if (colour_var == "%_practices") {
+      print(head(results_df))
+      print(summary(results_df))
+      print(glue::glue("Sum of %_RR_prev_summr_<5%: {sum(results_df['%_RR_prev_summr_<5%'], na.rm = TRUE)}"))
+      print(glue::glue("Sum of %_RR_prev_summr_>5%: {sum(results_df['%_RR_prev_summr_>5%'], na.rm = TRUE)}"))
+        results_df <- results_df %>%
+            pivot_longer(cols = c(glue("%_RR_{baseline}_summr_<5%"), glue("%_RR_{baseline}_summr_=1"), glue("%_RR_{baseline}_summr_>5%")), names_to = "%_practices", values_to = as.character(y_col))
+        
+        # Take the average across seasons for each year for the %_practices plot
+        results_df <- results_df %>%
+            group_by(measure, summer_year, `%_practices`) %>%
+            summarise(across(all_of(y_col), mean, na.rm = TRUE)) %>%
+            ungroup()
+    }
+  
+    # Format measure names for readability
+    results_df <- format_measures_for_plotting(results_df, set, subset)
+  
+    # Format colours for plotting
+    format_mappings <- format_colours_for_plotting(results_df, colour_var, y_var = y_var, baseline = baseline)    
+    linetype_mapping <- format_mappings$linetype_mapping
+    colour_mapping <- format_mappings$colour_mapping
+    results_df <- format_mappings$df
   
     line_plot <- results_df |>
         ggplot(aes(x = summer_year, y = .data[[y_col]], color = .data[[colour_var]], linetype = .data[[colour_var]])) +
@@ -294,7 +351,7 @@ plot_rr_timeseries_by_measure <- function(y_var, colour_var, baseline, weighting
         geom_point() +
         scale_linetype_manual(values = linetype_mapping) +
         scale_color_manual(values = colour_mapping) +
-        labs(title = glue("{y_var} (vs {baseline} summer) Over Time by {colour_var}"),
+        labs(title = glue("{y_var} (vs {baseline} summer) Over Time by {colour_var}, {weighting} by practice list size"),
             x = "year",
             y = y_var) +
         theme_minimal() +
@@ -304,28 +361,30 @@ plot_rr_timeseries_by_measure <- function(y_var, colour_var, baseline, weighting
     return(line_plot)
 }
 
-plot_variance_bar_chart <- function(var_type, set){
+plot_variance_bar_chart <- function(var_type, set, subset = NULL) {
     "This function produces a bar chart of the variance of the pressure metric
     Args:
         var_type: string name of the type of variance to plot (e.g. 'btwn' or 'w.in')
         set: Set of measures (resp or sro)"
     
     # Load variance df
-    variance_df <- read.csv(glue("output/practice_measures_{set}/practice_measures_{set}_DUMMY/Results_variances_test.csv"))
+    variance_df <- read.csv(glue("output/practice_measures_{set}/Results_variances.csv"))
 
-    variance_plot <- variance_df %>%
-      mutate(
-        season = factor(
-        season,
-        levels = c("Jun-Jul","Sep-Oct", "Nov-Dec", "Jan-Feb", "Mar-Apr")
-      )
-      ) %>%
+    # Format colours for plotting
+    format_mappings <- format_colours_for_plotting(variance_df, colour_var = "season")
+    linetype_mapping <- format_mappings$linetype_mapping
+    colour_mapping <- format_mappings$colour_mapping
+    variance_df <- format_mappings$df
+  
+    # Format measure names for readability
+    variance_plot <- format_measures_for_plotting(variance_df, set, subset) %>%
         ggplot(aes(x = measure, y = .data[[glue("var_rate_{var_type}_prac_season_median")]], fill = season)) +
         geom_col(position = position_dodge(width = 0.9)) +
         labs(title = glue("Variance in rate {var_type} practices by Season"),
             x = "Season",
             y = glue("Variance in rate {var_type} practices")) +
         theme_minimal() +
+        scale_fill_manual(values = colour_mapping) +
         theme(legend.title = element_blank())
     
     return(variance_plot)
